7bfadb320fd28e72e70a79bbed6862d85520621c
rodel1114 <rodelvillegas1114@gmail.com>
2015-04-27 16:49:26 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AMSS::Application.config.secret_token = '618dc2af33215d4f215cafa7c6679b30699c9d3e78489d3b1358ba88d226d8a1ef2965d2b6d07de9c7cabdbb622a390f04a6b0a9859df2fad69b432556ae53fe'
