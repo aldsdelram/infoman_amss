091c88176c94d48674ea13a516991ba02de9081f
Aldrin Ramos <aldrinramos013@gmail.com>
2015-04-28 10:33:49 +0800

    fixed another github errors

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
 AMSS::Application.config.secret_token = '5ee5d1a075dcbab513bcb86689ad1c26d1077ea99018a12c4f990019d7f5693484579ec66ceee4f0560de81d58430450c034767f44ba6c6e58a5d2daea8d32df'
-=======
-AMSS::Application.config.secret_token = '618dc2af33215d4f215cafa7c6679b30699c9d3e78489d3b1358ba88d226d8a1ef2965d2b6d07de9c7cabdbb622a390f04a6b0a9859df2fad69b432556ae53fe'
->>>>>>> e3f632c2ad50685aab59387ea9183d454616f53a
