e4b4e7d2fd9723971b7849be00949df2ca472481
Aldrin Ramos <aldrinramos013@gmail.com>
2015-05-06 14:41:01 +0800

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150505024817) do
+ActiveRecord::Schema.define(:version => 20150505064209) do
 
   create_table "applicants", :force => true do |t|
     t.string   "lastname"
