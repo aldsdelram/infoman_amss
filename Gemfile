c26ab773601eb39e0deabc1844fc6cde8d87ebf9
Aldrin Ramos <aldrinramos013@gmail.com>
2015-05-06 10:52:30 +0800

    pagination added to header search

--- Gemfile
+++ Gemfile
@@ -6,8 +6,6 @@ gem 'rails', '3.0.9'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2', '0.2.6'
-gem 'carrierwave'
-
 # Use unicorn as the web server
 # gem 'unicorn'
 
@@ -24,7 +22,9 @@ gem 'carrierwave'
 # gem 'sqlite3-ruby', :require => 'sqlite3'
 # gem 'aws-s3', :require => 'aws/s3'
 
+gem 'carrierwave'
 gem 'jquery-rails'
+gem 'will_paginate'
 
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -163,8 +163,24 @@ class ApplicantsController < ApplicationController
   end
 
   def header_search
-    @search = Applicant.find(:all, :conditions => ["firstname LIKE ? OR lastname LIKE ? OR middlename LIKE ?",
-     "%#{params[:applicant_name]}%", "%#{params[:applicant_name]}%", "%#{params[:applicant_name]}%"] )
+    @query = params[:applicant_name]
+    # @sql = "SELECT *" +
+    #        "FROM applicants" +
+    #        "WHERE CONCAT_WS(\' \',firstname,middlename,lastname) LIKE '%#{@query}%'" +
+    #        " OR CONCAT_WS(\' \',firstname,lastname) LIKE '%#{@query}%'"
+
+    # @search = Applicant.where("CONCAT_WS(\' \',firstname,middlename,lastname) LIKE ?" +
+      # " OR CONCAT_WS(\' \',firstname,lastname) LIKE ?" , "%#{@query}%", "%#{@query}%")
+
+    @search = Applicant.paginate(:conditions=> ["CONCAT_WS(\' \',firstname,middlename,lastname) LIKE ?" +
+      " OR CONCAT_WS(\' \',firstname,lastname) LIKE ?" , "%#{@query}%", "%#{@query}%"],
+        :page=>params[:page],
+        :order=>"lastname asc",
+        :per_page=> 5
+      )
+
+    # @search = Applicant.find_by_sql(@sql)
+
 
     respond_to do |format|
       format.html { render :action => 'header_search' }
@@ -186,7 +202,7 @@ class ApplicantsController < ApplicationController
 
   def assign_interviewer
     @applicant = Applicant.find(params[:id])
-    
+
   end
 
   def get_info
