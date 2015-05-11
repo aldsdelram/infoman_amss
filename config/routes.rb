16da80152b6fe69038fc8d37da9cbd1619af8451
Aldrin Ramos <aldrinramos013@gmail.com>
2015-05-11 11:02:07 +0800

    glyphicon added, applicants index modified, applicant show info modified

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ AMSS::Application.routes.draw do
   controller :applicants do
     con = 'applicants/'
     get con+'show_all' => :show_all
-    post con+'show_all' => :show_all
+    post con+'show_all' => :show_all, :as => 'show_all'
     get 'search_applicant' => :search_applicant
     get con+'header_search' => :header_search
     get 'assign_interviewer/:id' => :assign_interviewer, :as => con+'assign_interviewer'
