10fa878997b2dfd1b86b814e023d16620aaa4067
Aldrin Ramos <aldrinramos013@gmail.com>
2015-05-12 16:12:34 +0800

    home index modified

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   def index
     unless Admin.find_by_id(session[:admin_id])
       redirect_to login_url
+    else
+      @admin = Admin.find(session[:admin_id])
     end
   end
 
