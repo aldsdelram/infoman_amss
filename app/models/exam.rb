7f6579c6e9822408d432fd4e1b2548196efc3b80
Aldrin Ramos <aldrinramos013@gmail.com>
2015-05-14 13:10:51 +0800

    modified

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,6 +1,15 @@
 class Exam < ActiveRecord::Base
   has_many :exam_position_assignment
   has_many :positions, :through => :exam_position_assignment
-
+  has_many :grades
   validates :title, :presence => true, :uniqueness=>true
+  validates :total_score, :passing_score, :presence=> true
+  validates :total_score, :numericality => { only_integer: true,
+                           greater_than: 0}
+  validates :passing_score, :numericality => { only_integer: true,
+                           greater_than: 0}
+  validates :passing_score, :numericality => {
+                          less_than_or_equal_to: :total_score,
+                          :message=> "must be less than or equal" +
+                          " to the Total Score"}
 end
--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,2 +1,4 @@
 class Grade < ActiveRecord::Base
+  belongs_to :exam
+  belongs_to :applicant
 end
